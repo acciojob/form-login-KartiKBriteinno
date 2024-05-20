7dbe36458657adfe54a93abad74cce06eb5bbdb5
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-20 16:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function getFormvalue(event) {
     // Prevent the default form submission
     event.preventDefault();
     
-    // Retrieve the values from the input fields
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
+    // Retrieve the values from the input fields using their name attributes
+    var firstName = document.querySelector("input[name='fname']").value;
+    var lastName = document.querySelector("input[name='lname']").value;
     
     // Concatenate the first name and last name
     var fullName = firstName + " " + lastName;
