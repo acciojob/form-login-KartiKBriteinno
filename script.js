7f931269dfe6a7f45eab7be3ac71820dce70ae79
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-20 16:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    
+    // Retrieve the values from the input fields
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    
+    // Concatenate the first name and last name
+    var fullName = firstName + " " + lastName;
+    
+    // Display the full name in an alert
+    alert(fullName);
 }
